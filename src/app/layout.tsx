937bb5d2d981b03280fc24b2ff734ab7701725d9
AboutSelphy <aboutselphy@gmail.com>
2024-11-09 05:00:29 +0100

    adding scss and config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import  "style/global.scss"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// !Imports
+import { Chivo, Akshar } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const chivo = Chivo({
+  subsets: ['latin'],
+  variable: '--font-chivo',
+  display: 'swap',
+})
+
+const akshar = Akshar({
+  subsets: ['latin'],
+  variable: '--font-akshar',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,8 +28,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${chivo.variable} ${akshar.variable}`}>
+      <body>
         {children}
       </body>
     </html>
